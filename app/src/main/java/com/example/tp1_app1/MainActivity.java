22faaf1d8a98235dde826d32229784d095dfec40
tb59 <thomas.bourdeaudhuy@gmail.com>
2020-05-03 23:21:36 +0200

    App1 finale

--- app/src/main/java/com/example/tp1_app1/MainActivity.java
+++ app/src/main/java/com/example/tp1_app1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.tp1_app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "PMR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onReStart");
     }
 }
